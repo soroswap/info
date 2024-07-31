0f62a604739d73a9dfb0370048c3aa1dddc4b265
abstract829 <maxxii.1820@gmail.com>
2024-07-31 09:10:46 -0400

    sort tvl chart by date

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,10 +64,16 @@ export default function Home() {
       });
     });
 
-    return Object.keys(tvlChartData).map((key) => ({
+    const result = Object.keys(tvlChartData).map((key) => ({
       date: tvlChartData[key].date,
       tvl: tvlChartData[key].tvl,
     }));
+
+    const orderbyDate = result.sort((a, b) => {
+      return new Date(a.date).getTime() - new Date(b.date).getTime();
+    });
+
+    return orderbyDate;
   };
 
   return (
